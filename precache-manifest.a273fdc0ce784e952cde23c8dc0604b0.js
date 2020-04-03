b76d3596346740a91c136edb0280aac83be4095e
jp631 <47936264+jp631@users.noreply.github.com>
2020-04-02 22:21:07 -0400

    Updates

---
+++ precache-manifest.a273fdc0ce784e952cde23c8dc0604b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cb531517ed97243eb04b21411578bf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c8189c738aede4e5a8ac",
+    "url": "/monster-rolodex/static/css/main.3b170665.chunk.css"
+  },
+  {
+    "revision": "95f4ad7765953f44ba62",
+    "url": "/monster-rolodex/static/js/2.001fd4be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.001fd4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8189c738aede4e5a8ac",
+    "url": "/monster-rolodex/static/js/main.6ad40034.chunk.js"
+  },
+  {
+    "revision": "0d6e087be5b03928614d",
+    "url": "/monster-rolodex/static/js/runtime-main.8aedef64.js"
+  }
+]);
